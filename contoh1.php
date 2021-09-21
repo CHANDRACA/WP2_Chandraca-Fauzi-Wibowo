190264a7d1ce24c3ed07ba978a86f40e33117e20
CHANDRACA <90746236+CHANDRACA@users.noreply.github.com>
2021-09-21 12:33:50 +0700

    Add files via upload

---
+++ contoh1.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controllers;
+
+class Contoh1 extends BaseController
+{
+    public function index()
+    {
+        echo "<h1>PERKENALKAN</h1>";
+        echo "Nama saya Chandraca Fauzi Wibowo, Saya tinggal di Bekasi , olah raga yang saya sukai adalah Sepakbola";
+    }
+}
