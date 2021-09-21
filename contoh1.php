eae90321df7dd96c7ac03b7a0f97452e54350e32
CHANDRACA <90746236+CHANDRACA@users.noreply.github.com>
2021-09-21 12:49:53 +0700

    Add files via upload

--- contoh1.php
+++ contoh1.php
@@ -7,6 +7,8 @@ class Contoh1 extends BaseController
     public function index()
     {
         echo "<h1>PERKENALKAN</h1>";
-        echo "Nama saya Chandraca Fauzi Wibowo, Saya tinggal di Bekasi , olah raga yang saya sukai adalah Sepakbola";
+        echo "Nama saya Chandraca Fauzi Wibowo 
+            Saya tinggal di Bekasi  
+            Olahraga yang saya sukai adalah Sepakbola";
     }
 }
